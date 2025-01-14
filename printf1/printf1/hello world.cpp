7aa91a9eee9f77399a6695698cba391f03b2aea7
赵阳 <3540659906@qq.com>
2025-01-14 18:01:28 +0800

    更新

--- printf1/printf1/hello world.cpp
+++ printf1/printf1/hello world.cpp
@@ -2576,33 +2576,35 @@
 //
 //}
 
-#include<stdio.h>
-#include<string.h>
 
-void left_move(char arr[], int k)
-{
-	int i = 0;
-	int len = strlen(arr);
-	for (i = 0; i < k; i++)
-	{
-		char tmp = arr[0];
-		int j = 0;
-		for (j = 0; j < len - 1; j++)
-		{
-			arr[j] = arr[j + 1];
-		}
-		arr[len - 1] = tmp;
-	}
-}
-int main()
-{
-	char arr[] = "abcdef";
-	int k = 2;
-	left_move(arr, k);
-	printf("%s\n", arr);
-	return 0;
-}
-
-
-
-
+//×óÐý×ª
+//#include<stdio.h>
+//#include<string.h>
+//
+//void left_move(char arr[], int k)
+//{
+//	int i = 0;
+//	int len = strlen(arr);
+//	for (i = 0; i < k; i++)
+//	{
+//		char tmp = arr[0];
+//		int j = 0;
+//		for (j = 0; j < len - 1; j++)
+//		{
+//			arr[j] = arr[j + 1];
+//		}
+//		arr[len - 1] = tmp;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int k = 2;
+//	left_move(arr, k);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//
+//
+//
+//
